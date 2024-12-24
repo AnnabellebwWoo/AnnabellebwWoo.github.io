2a6ff54c2930474d1cb0e2cd58c5f1e202317b28
Annabelle <annabelle.bw.woo@gmail.com>
2024-12-23 23:50:47 -0500

    added footer and intro, updated logo and navbar

--- belle-reve-blog/src/app/page.tsx
+++ belle-reve-blog/src/app/page.tsx
@@ -1,13 +1,17 @@
 import Logo from '@/components/Logo/Logo';
 import NavBar from '../components/NavBar/NavBar';
 import BlogCard from '../components/BlogCard/BlogCard'
+import Intro from '@/components/Intro/Intro';
+import Footer from '@/components/Footer/Footer';
 
 export default function Home() {
   return (
     <>
       <Logo></Logo>
       <NavBar></NavBar>
+      <Intro></Intro>
       <BlogCard></BlogCard>
+      <Footer></Footer>
     </>
   );
 }
