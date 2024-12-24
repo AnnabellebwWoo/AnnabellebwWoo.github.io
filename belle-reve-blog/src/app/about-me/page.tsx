2d22370201c50f2c489afaf204e7a568b9579c60
Annabelle <annabelle.bw.woo@gmail.com>
2024-12-24 03:38:38 -0500

    updated layout, added about me page

---
+++ belle-reve-blog/src/app/about-me/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './styles.module.css'
+
+const AboutPage = () => {
+  return (
+    <div className={styles.about}>
+      <Image src="/images/annabelle-woo.jpg" alt="Annabelle Woo" width={300} height={400}></Image>
+      <p>Belle Reve is a blog created by Annabelle Woo. Magazines and websites were my favourite mediums for learning about fashion, beauty, and current events. After frequenting my favourite sites like Vogue, Harper’s Bazaar, and Into The Gloss, I decided to create a blog.
+        Belle Reve was born to express all of my interests. Varying from fashion and beauty to baking and trying new things. I hope you enjoy digesting the content as much as I like creating it.</p>
+    </div>
+  )
+}
+
+export default AboutPage
