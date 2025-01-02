04842f3548a714cf1e167862947e8b5c473b506f
Annabelle <annabelle.bw.woo@gmail.com>
2025-01-02 18:15:45 -0500

    created blog card and types

--- belle-reve-blog/src/app/page.tsx
+++ belle-reve-blog/src/app/page.tsx
@@ -1,11 +1,24 @@
 import BlogCard from '../components/BlogCard/BlogCard'
 import Intro from '@/components/Intro/Intro';
+import { getAllPosts } from '@/lib/utils'
+import type { GetStaticProps } from 'next'
+// import { BlogProps } from "@/lib/types";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Intro></Intro>
       <BlogCard></BlogCard>
-    </>
+    </div>
   );
+}
+
+export async function GetStaticProps() {
+  const posts = await getAllPosts()
+
+  return{
+    props: {
+      posts
+    }
+  }
 }
