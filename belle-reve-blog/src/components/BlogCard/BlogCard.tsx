4f68778c0dc3f75145e46f289f22c99db6593f71
Annabelle <annabelle.bw.woo@gmail.com>
2025-01-02 18:15:45 -0500

    created blog card and types

--- belle-reve-blog/src/components/BlogCard/BlogCard.tsx
+++ belle-reve-blog/src/components/BlogCard/BlogCard.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import styles from './BlogCard.module.css';
+import Link from "next/link";
+// import { BlogPostsProps } from "@/lib/types";
 
-const BlogCard= () => {
+const BlogCard= ( ) => {
     return(
        <>
         <div className={styles.row}>
-            <div className={styles.column} style={{backgroundColor: 'red'}}>
-                <div>Column 1</div>
+            <div className={styles.column}>
+                    
+                 <ul className={styles.card}>
+                    <h1><Link href="/blog-post">Title</Link></h1>
+                   <p className={styles.excerpt}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <hr className={styles.Line}></hr>
+                    <ul className={styles.moreInfo}>
+                        <li><Link href="/category/beauty">category</Link></li>
+                        <li><p>01/02/2025</p></li>
+                    </ul>
+                    <hr className={styles.Line}></hr>
+                  </ul>
+
                 </div>
             <div className={styles.column} style={{backgroundColor: 'green'}}>
                     <div>Column 2</div>
